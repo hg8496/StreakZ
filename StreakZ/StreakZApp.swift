577d586a2ec8018ccc7c6fd80558d564753b0100
Christian Stolz <hg8496@cstolz.de>
2024-08-27 17:52:16 +0200

    Initial Commit

---
+++ StreakZ/StreakZApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakZApp.swift
+//  StreakZ
+//
+//  Created by Christian Stolz on 27.08.24.
+//
+
+import SwiftUI
+
+@main
+struct StreakZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
